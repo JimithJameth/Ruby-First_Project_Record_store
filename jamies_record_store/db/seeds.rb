31a966b01f38955772f704418160e8fea4466b53
Jamie Martin <jamiemartinmusic@gmail.com>
2017-06-18 14:50:19 +0100

    . albums and .artist working

--- jamies_record_store/db/seeds.rb
+++ jamies_record_store/db/seeds.rb
@@ -2,16 +2,18 @@ require 'pry-byebug'
 require_relative('../models/album')
 require_relative('../models/artist')
 require_relative('../models/genre')
-Artist.delete_all()
 Album.delete_all()
+Artist.delete_all()
 
 artist1 = Artist.new({
   "name" => "David Bowie"
   })
+artist1.save()
 
 artist2 = Artist.new({
   "name" => "XTC"
   })
+artist2.save()
 
   
 album1 = Album.new({
@@ -19,15 +21,14 @@ album1 = Album.new({
   "artist_id" => artist1.id
   })
 
+album1.save()
+
 album2 = Album.new({
   "title" => "Drums and Wires",
-  "artist_id" => artist1.id
+  "artist_id" => artist2.id
   })
-
-artist1.save()
-album1.save()
-artist2.save()
 album2.save()
-artist1.albums()
+
+
 binding.pry
 nil
