8df8c78d13edd1533bb7b690c55ee48174981dc3
Jamie Martin <jamiemartinmusic@gmail.com>
2017-06-19 10:02:33 +0100

    Genre delete method added

--- jamies_record_store/db/recordstore.sql
+++ jamies_record_store/db/recordstore.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS genres;
 DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS genres;
 DROP TABLE IF EXISTS artists;
 
 CREATE TABLE artists(
@@ -7,16 +7,16 @@ CREATE TABLE artists(
   name VARCHAR(255)
 );
 
-CREATE TABLE albums(
-  id serial8 PRIMARY KEY,
-  title VARCHAR(255),
-  artist_id INT8 REFERENCES artists(id),
-  genre_id INT8 REFERENCES genres(id)
-);
 
 CREATE TABLE genres(
   id serial8 PRIMARY KEY,
   type VARCHAR(255)
 );
 
-
+CREATE TABLE albums(
+  id serial8 PRIMARY KEY,
+  title VARCHAR(255),
+  quantity INT8,
+  artist_id INT8 REFERENCES artists(id) ON DELETE CASCADE,
+  genre_id INT8 REFERENCES genres(id)
+);
--- jamies_record_store/db/seeds.rb
+++ jamies_record_store/db/seeds.rb
@@ -3,6 +3,7 @@ require_relative('../models/album')
 require_relative('../models/artist')
 require_relative('../models/genre')
 Album.delete_all()
+Genre.delete_all()
 Artist.delete_all()
 
 
@@ -21,31 +22,52 @@ artist3 = Artist.new({
   })
 artist3.save()
 
+artist4 = Artist.new({
+  "name" => "Peter Gabriel"
+  })
+artist4.save()
+
 genre1 = Genre.new({
-  "genre" => "pop"
+  "type" => "pop"
   })
 genre1.save()
+
+genre2 = Genre.new({
+  "type" => "indie rock"
+  })
+genre2.save()
 
 album1 = Album.new({
   "title" => "Heros",
   "genre_id" => genre1.id,
-  "artist_id" => artist1.id
+  "artist_id" => artist1.id,
+  "quantity" => 15
   })
 album1.save()
 
 album2 = Album.new({
   "title" => "Drums and Wires",
   "artist_id" => artist2.id,
-  "genre_id" => genre1.id
+  "genre_id" => genre1.id,
+  "quantity" => 4
   })
 album2.save()
 
 album3 = Album.new({
   "title" => "No wow",
   "artist_id" => artist3.id,
-  "genre_id" => genre1.id
+  "genre_id" => genre2.id,
+  "quantity" => 7
   })
 album3.save()
+
+album4 = Album.new({
+  "title" => "So",
+  "artist_id" => artist4.id,
+  "genre_id" => genre1.id,
+  "quantity" => 10
+  })
+album4.save()
 
 binding.pry
 nil
